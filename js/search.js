be218cc328703619ca66550cd5e10e98e20fcda2
Elora <elora.delpierre@gmail.com>
2021-05-10 16:46:25 +0200

    affichage aucun résultat + recherche à partir de 3 lettres

--- js/search.js
+++ js/search.js
@@ -5,48 +5,53 @@ function searchBar() {
   const resultGallery = document.getElementById("result");
   const searchBarInput = document.getElementById("search__bar__input");
   let searchValue = searchBarInput.value;
-  console.log(searchValue);
-  for (let recipe of recipes) {
-    let nameArray = recipe.name.split(" ");
-    let descArray = recipe.description.split(" ");
-    let ingredientsArray = recipe.ingredients.map((ing) => {
-      return ing.ingredient;
-    });
-    if (nameArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-      // choisir l'ordre qui a le plus de chance d'abouttir a un true rapidement//
-      console.log("ça marche");
-      recipesToDisplay.push(recipe);
-    } else if (descArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-      console.log("ça marche");
-      recipesToDisplay.push(recipe);
-    } else if (ingredientsArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-      recipesToDisplay.push(recipe);
-    }
-    console.log(recipesToDisplay.length);
-    if (recipesToDisplay.length == 0) {
-      resultGallery.innerText = "Aucun résultat";
+  if (searchValue.length >= 3) {
+    for (let recipe of recipes) {
+      let nameArray = recipe.name.split(" ");
+      let descArray = recipe.description.split(" ");
+      let ingredientsArray = recipe.ingredients.map((ing) => {
+        return ing.ingredient;
+      });
+      if (nameArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+        // choisir l'ordre qui a le plus de chance d'abouttir a un true rapidement//
+
+        recipesToDisplay.push(recipe);
+      } else if (descArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+        recipesToDisplay.push(recipe);
+      } else if (ingredientsArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+        recipesToDisplay.push(recipe);
+      }
+      console.log(recipesToDisplay.length);
     }
   }
-
   resultGallery.innerHTML = "";
+  console.log(recipesToDisplay);
   displayRecipes(recipesToDisplay);
+  const ingredientTagInput = document.getElementById("search__sort__ingredients__input");
+  ingredientTagInput.addEventListener("click", function () {
+    const ulIngredient = document.getElementById("search__sort__ingredients__ul");
+    ulIngredient.innerHTML = "";
+    ingredientTagSearch(recipesToDisplay);
+  });
 }
 
 function ingredientTagSearch(recipesArray) {
   let allIngredients = [];
-  let allIngredientSplit = [];
   for (let recipe of recipesArray) {
     allIngredients.push(
       recipe.ingredients.map((ing) => {
+        // mettre dans le tableau allIngredients tous les ingrédients splités//
         return ing.ingredient.split(" ");
       })
     );
   }
-  allIngredients = allIngredients.flat(2);
-  console.log(allIngredients);
-  let uniqueIngredientsArray = Array.from(new Set(allIngredients));
+
+  allIngredients = allIngredients.flat(2); // supprimer les profondeurs de tableau sur 2 niveaux//
+
+  let uniqueIngredientsArray = Array.from(new Set(allIngredients)); //enlever les doublons//
   console.log(uniqueIngredientsArray);
   for (let ingredient of uniqueIngredientsArray) {
+    console.log(ingredient);
     const ulIngredient = document.getElementById("search__sort__ingredients__ul");
     const liIngredient = document.createElement("li");
     ulIngredient.appendChild(liIngredient);
@@ -54,6 +59,7 @@ function ingredientTagSearch(recipesArray) {
     linkIngredient.setAttribute("class", "search__sort__ingredients__ul__li dropdown-item");
     linkIngredient.setAttribute("href", "#");
     linkIngredient.innerHTML = ingredient;
+    liIngredient.appendChild(linkIngredient);
   }
 }
 
@@ -61,32 +67,36 @@ function displayRecipes(recipesArray) {
   const resultGallery = document.getElementById("result");
   const searchBarInput = document.getElementById("search__bar__input");
   searchBarInput.addEventListener("input", searchBar);
-  const ingredientTagInput = document.getElementById("search__sort__ingredients__input");
-  ingredientTagInput.addEventListener("click", function () {
-    ingredientTagSearch(recipesArray);
-  });
-  for (let recipe of recipesArray) {
-    const recipesName = recipe.name;
-    const recipesIngredients = recipe.ingredients;
-    const recipesTime = recipe.time;
-    const recipesDesc = recipe.description;
-    const recipesAppliance = recipe.appliance;
-    const recipesUstensils = recipe.ustensils;
-    const recipeCol = document.createElement("div");
-    recipeCol.setAttribute("class", "col");
-    resultGallery.appendChild(recipeCol);
-    const recipeCard = document.createElement("div");
-    recipeCard.setAttribute("class", "recipe ");
-    recipeCol.appendChild(recipeCard);
-    addImgInRecipe(recipeCard);
-    const recipeInfo = document.createElement("div");
-    recipeInfo.setAttribute("class", "recipe__info ");
-    recipeCard.appendChild(recipeInfo);
-    addTitleInRecipe(recipeInfo, recipesName, recipesTime);
-    addIngredientsInrecipe(recipesIngredients, recipeInfo);
-    addDescInRecipe(recipeInfo, recipesDesc);
-    addApplianceInRecipe(recipeInfo, recipesAppliance);
-    addUstensilsInRecipe(recipeInfo, recipesUstensils);
+
+  if (recipesArray.length == 0) {
+    const noResult = document.createElement("p");
+    noResult.setAttribute("id", "result__no");
+    noResult.innerText = "Aucun résultat";
+    resultGallery.appendChild(noResult);
+  } else {
+    for (let recipe of recipesArray) {
+      const recipesName = recipe.name;
+      const recipesIngredients = recipe.ingredients;
+      const recipesTime = recipe.time;
+      const recipesDesc = recipe.description;
+      const recipesAppliance = recipe.appliance;
+      const recipesUstensils = recipe.ustensils;
+      const recipeCol = document.createElement("div");
+      recipeCol.setAttribute("class", "col");
+      resultGallery.appendChild(recipeCol);
+      const recipeCard = document.createElement("div");
+      recipeCard.setAttribute("class", "recipe ");
+      recipeCol.appendChild(recipeCard);
+      addImgInRecipe(recipeCard);
+      const recipeInfo = document.createElement("div");
+      recipeInfo.setAttribute("class", "recipe__info ");
+      recipeCard.appendChild(recipeInfo);
+      addTitleInRecipe(recipeInfo, recipesName, recipesTime);
+      addIngredientsInrecipe(recipesIngredients, recipeInfo);
+      addDescInRecipe(recipeInfo, recipesDesc);
+      addApplianceInRecipe(recipeInfo, recipesAppliance);
+      addUstensilsInRecipe(recipeInfo, recipesUstensils);
+    }
   }
 }
 
